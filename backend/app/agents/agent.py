8a982aafb495bbbbba9eed75302fd829ef8b99bc
tushar00045 <goyaltushar621@gmail.com>
2026-05-21 17:18:39 +0530

    Modified Agent.py

--- backend/app/agents/agent.py
+++ backend/app/agents/agent.py
@@ -1,12 +1,27 @@
-from openai import OpenAI
+#from openai import OpenAI
+from google import genai
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-client=OpenAI(
-  api_key=os.getenv("OPENAI_API_KEY")
+# client=OpenAI(
+#   api_key=os.getenv("OPENAI_API_KEY")
+# )
+
+client = genai.Client(
+    api_key=os.getenv("GEMINI_API_KEY")
 )
+
+def ask_agent(prompt: str):
+
+    response = client.models.generate_content(
+        model="gemini-3.5-flash",
+        contents=prompt,
+    )
+
+    return response.text
+
 
 # def ask_agent(prompt:str):
 #   response=client.chat.completions.create(
@@ -24,5 +39,5 @@ client=OpenAI(
 #   )
 #   return response.choices[0].message.content
 
-def ask_agent(prompt: str):
-    return f"Agent Firewall received: {prompt}"
+# def ask_agent(prompt: str):
+#     return f"Agent Firewall received: {prompt}"
